01336ecea11c4ca04fec2735a02c0f68f856df7a
Darian-Frey <shane.hartley06@gmail.com>
2021-09-05 22:21:36 +0000

    Added increase/decrease update/remove to bag area

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -18,8 +18,34 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
-        messages.success(request, "Successfully added to your bag")
+        messages.success(request, "Item added to your bag")
 
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
+
+def adjust_bag(request, item_id):
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity in range(0, 10):
+        bag[item_id] = quantity
+        messages.success(request, "Item added to your bag")
+    else:
+        bag.pop(item_id)
+        messages.success(request, "Item removed from you bag")
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    product = get_object_or_404(Product, pk=item_id)
+    bag = request.session.get('bag', {})
+
+    bag.pop(item_id)
+    messages.success(request, "Item removed from your bag")
+
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
