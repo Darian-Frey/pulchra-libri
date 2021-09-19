6f4e34cc2251ae8737786f3f7f8cb7cae47fecb5
Darian-Frey <shane.hartley06@gmail.com>
2021-09-19 18:56:26 +0000

    Update allauth email, now working

--- pulchra_libri/settings.py
+++ pulchra_libri/settings.py
@@ -179,7 +179,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'pulchra-libri@store.co.uk'
+    DEFAULT_FROM_EMAIL = 'pulchra-libri@gmail.co.uk'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
